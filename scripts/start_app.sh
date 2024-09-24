7ed55cc2979a328040ecb3b54a2cf73c8a8de963
Jordon Douglas <jordondouglas@Jordons-MacBook-Air.local>
2024-09-24 08:56:04 -0500

    adding start up script

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 git clone https://github.com/jordondoug2019/microblog_VPC_deployment.git
+sudo apt install python3-pip
+sudo apt install python3-flask
+sudo apt install gunicorn
 pip install -r requirements.txt
 pip install gunicorn pymysql cryptography
 FLASK_APP=microblog.py
